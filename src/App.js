b145b53b83c61c3c60b04efabf791f29abbe2f18
alihasan13 <ali.hasan@northsouth.edu>
2023-06-04 01:24:48 +0600

    landing page decoration done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import { Toaster } from 'react-hot-toast';
-// import AppContent from './components/AppContent';
 import AppHeader from './components/Header';
-// import PageTitle from './components/PageTitle';
-// import styles from './styles/modules/app.module.scss';
+import PageTitle from './components/PageTitle';
 
 function App() {
   return (
     <>
       <div className="container">
         <PageTitle>TODO List</PageTitle>
-        <div className={styles.app__wrapper}>
-          <AppHeader />
-          <AppContent />
-        </div>
+         
+          
       </div>
-      <Toaster
-        position="bottom-right"
-        toastOptions={{
-          style: {
-            fontSize: '1.4rem',
-          },
-        }}
-      />
+     
     </>
   );
 }
