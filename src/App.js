cb112c3edbdcbf6a321f00c727e4d84094fa5bf9
alihasan13 <ali.hasan@northsouth.edu>
2023-06-04 22:07:07 +0600

    worked on buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import AppHeader from './components/Header';
-import PageTitle from './components/PageTitle';
+import React from "react";
+import Header from "./components/Header";
+import PageTitle from "./components/PageTitle";
+import style from  "./styles/modules/app.module.scss";
 
 function App() {
   return (
     <>
       <div className="container">
         <PageTitle>TODO List</PageTitle>
-         
-          
+        <div className={style.app__wrapper}>
+          <Header/>
+        </div>
       </div>
-     
     </>
   );
 }
--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from '../styles/modules/title.module.scss'
 
-function PageTitle({children}) {
+const PageTitle =({children}) => {
   return (
     <p className={style.title}>{children}</p>
   )
